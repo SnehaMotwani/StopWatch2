d5b1233341b9c694dc5da43e4a63283a90a49a44
Sneha Motwani <snehamotwani11@gmail.com>
2020-08-30 01:49:52 +0530

    Final Commit

---
+++ StopWatch2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using StopWatch2;
+
+namespace StopWatch2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StopWatch stopWatch = new StopWatch();
+
+            stopWatch.Start();
+
+            Random random = new Random();       //random wait time...upto 7 seconds
+            int wait = random.Next(7);
+            Thread.Sleep(wait * 1000);
+
+            stopWatch.Stop();
+
+            Console.WriteLine("Interval: " + stopWatch.TimeInterval());
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ StopWatch2/StopWatch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace StopWatch2
+{
+    public class StopWatch
+    {
+        DateTime StartTime
+        {
+            get;
+            set;
+        }
+        DateTime EndTime
+        {
+            get;
+            set;
+        }
+        bool CheckIfStarted
+        {
+            get;
+            set;
+        }
+
+        public void Start()
+        {
+            if (!CheckIfStarted)
+            {
+                StartTime = DateTime.Now;
+                CheckIfStarted = true;
+            }
+        }
+        public void Stop()
+        {
+            if (CheckIfStarted)
+            {
+                EndTime = DateTime.Now;
+                CheckIfStarted = false;
+            }
+        }
+        public TimeSpan TimeInterval()
+        {
+            return EndTime - StartTime;
+        }
+    }
+}
